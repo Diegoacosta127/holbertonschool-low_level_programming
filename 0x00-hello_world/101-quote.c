082880784ec1922ddf34c8969beb438ccdc69c19
Diegoacosta127 <diegoacosta127@gmail.com>
2021-09-30 11:51:26 -0700

    9. UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - the main
+ * Return: always 1
+ */
+int main (void)
+{
+	print_string("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
