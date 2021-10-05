3041696bcf41ddcfabf68343fedd7023d98504d9
Diegoacosta127 <diegoacosta127@gmail.com>
2021-10-05 09:46:10 -0700

    5. Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,7 +12,9 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	}
-	else if (n < 0)
+	else
+	{
+		if (n < 0)
 		{
 			_putchar('-');
 			return (-1);
@@ -22,4 +24,5 @@ int print_sign(int n)
 			_putchar(48);
 			return (0);
 		}
+	}
 }
