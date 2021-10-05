17389ab1266aae2ff197cbafe369e9a72d379fe0
Diegoacosta127 <diegoacosta127@gmail.com>
2021-10-05 13:09:54 -0700

    11. 98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /**
-  * print_to_98: prints all naturals from n to 98, followed by a new line
+  * print_to_98 - prints all naturals from n to 98, followed by a new line
+  * @n: The input number
   * Return: void
   */
 void print_to_98(int n);
 {
-	while(n!=98)
+	while (n != 98)
 	{
 		printf("%d, ", n);
 		if (n > 98)
