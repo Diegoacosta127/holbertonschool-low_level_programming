6e74734b2e4ea4cf4ea18b47a324227976d576a0
Diegoacosta127 <diegoacosta127@gmail.com>
2021-10-12 09:17:25 -0700

    0. 98 Battery St.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-  * reset_to_98 - takes a pointer to an int and updates the vault it point to 98
+  * reset_to_98 - takes a pointer to an int and updates the vault 98
   * @n: the pointer
   * Return: void
   */
