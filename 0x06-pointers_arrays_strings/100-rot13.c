a677e2a063a3ebc3e444d7e7852584d4099e4b13
Diegoacosta127 <diegoacosta127@gmail.com>
2021-10-16 20:41:31 -0700

    8. rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+  * rot13 - encodes a string using rot13
+  * @s: string to be encoded
+  * Return: string encoded
+  */
+char *rot13(char *s)
+{
+	int i, j;
+	char rot[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	char alpha[] = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	i = 0;
+	while (*(s + i) != 0)
+	{
+		for (j = 0; alpha[j] != '\0'; j++)
+		{
+			if (*(s + i) == alpha[j])
+				*(s + i) = rot[j];
+		}
+		i++;
+	}
+	return (s);
+}
