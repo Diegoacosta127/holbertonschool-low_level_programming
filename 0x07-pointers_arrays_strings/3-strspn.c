35e7e2d3a229540a2ecf2e82a4377f9be2569655
Diegoacosta127 <diegoacosta127@gmail.com>
2021-10-18 10:36:22 -0700

    3. strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+  * _strspn - gets the length of a prefix substring
+  * @s: string to be analized
+  * @accept: characters to be searched in s
+  * Return: length of characters found
+  */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j;
+	int len;
+
+	len = 0;
+
+	for (i = 0; *(s + i) != 0; i++)
+	{
+		for (j = 0; *(accept + j) != 0; j++)
+		{
+			if (*(s + i) == *(accept + j))
+			{
+				len++;
+				break;
+			}
+		}
+	}
+	return (len - 2);
+}
