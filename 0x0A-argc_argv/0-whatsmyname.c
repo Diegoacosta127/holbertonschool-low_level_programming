d8fa5695a1e2145bd6bb1457011ef3af9e33ea53
Diegoacosta127 <diegoacosta127@gmail.com>
2021-10-22 05:46:24 -0700

    0. It ain't what they call you, it's what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+  * main - prints the name of the executable file
+  * @argc: argument counter
+  * @argv: argument vector|
+  */
+int main(int argc, char *argv[])
+{
+	argc = 0;
+	printf("%s\n", argv[0]);
+	return (argc);
+}
