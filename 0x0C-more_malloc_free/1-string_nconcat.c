eb53c97fbaff6273585a4878c109a0570edc52e8
Diegoacosta127 <diegoacosta127@gmail.com>
2021-10-28 08:48:54 -0700

    1. str_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -26,9 +26,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		ls2++;
 	}
+	ls2 += 1;
 	if (n > ls2)
 	{
-		n = ls2 - 1;
+		n = ls2;
 	}
 	p = malloc(ls1 + n + 1);
 	if (p == NULL)
