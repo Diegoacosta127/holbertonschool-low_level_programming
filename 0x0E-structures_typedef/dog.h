d799df32e3e5f780e5a3496907f86361a5842ba5
Diegoacosta127 <diegoacosta127@gmail.com>
2021-11-01 08:59:13 -0700

    Betty fix - comments for members

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
+/**
+  * struct dog - name, age and owner of the dog
+  * @name: name of the dog
+  * @age: age of the puppy
+  * @owner: name of it owner
+  */
 struct dog
 {
 	char *name;
