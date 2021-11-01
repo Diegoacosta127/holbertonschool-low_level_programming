b5982e76b0252824e94be0f8b4e39add531a94ef
Diegoacosta127 <diegoacosta127@gmail.com>
2021-11-01 12:18:23 -0700

    Extra functions deleted

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,59 +3,6 @@
 #include <stdio.h>
 #include <string.h>
 /**
-  * _strlen - length of a string
-  * @s: pointer to char
-  * Return: length of a string
-  */
-int _strlen(char *s)
-{
-	int i;
-
-	i = 0;
-
-	while (*s != '\0')
-	{
-		i++;
-		s++;
-	}
-	return (i);
-}
-/**
-  * _strdup - returns pointer to newly allocated space in memo
-  * @str: string to be copied
-  * Return: pointer to memo. alloc., NULL if str = NULL or if insufficiente mem
-  */
-char *_strdup(char *str)
-{
-	int i;
-	char *p;
-
-	if (str == NULL)
-	{
-		return (NULL);
-	}
-	p = malloc((sizeof(char) * _strlen(str)) + 1);
-	if (p == NULL)
-	{
-		return (NULL);
-	}
-	i = 0;
-	if (*str == '\0')
-	{
-		*p = '\0';
-		return (p);
-	}
-	else
-	{
-		while (*(str + i) != '\0')
-		{
-			*(p + i) = *(str + i);
-			i++;
-		}
-	}
-	return (p);
-}
-/**
   * new_dog - creates a new dog :)
   * @name: name of the dog
   * @age: age of the puppy
@@ -69,13 +16,13 @@ dog_t *new_dog(char *name, float age, char *owner)
 	d = malloc(sizeof(dog_t));
 	if (!d)
 		return (NULL);
-	d->name = _strdup(name);
+	d->name = strdup(name);
 	if (!(d->name))
 	{
 		free(d);
 		return (NULL);
 	}
-	d->owner = _strdup(owner);
+	d->owner = strdup(owner);
 	if (!(d->owner))
 	{
 		free(d->name);
