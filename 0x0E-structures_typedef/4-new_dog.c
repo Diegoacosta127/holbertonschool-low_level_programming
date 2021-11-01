06405eb1cbd6fb93ac801dcd61dbaffe2a9d182d
Diegoacosta127 <diegoacosta127@gmail.com>
2021-11-01 12:23:38 -0700

    _strlen added (again)

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,7 +1,41 @@
 #include "dog.h"
 #include <stdlib.h>
-#include <stdio.h>
 #include <string.h>
+/**
+  * _strdup - returns pointer to newly allocated space in memo
+  * @str: string to be copied
+  * Return: pointer to memo. alloc., NULL if str = NULL or if insufficiente mem
+  */
+char *_strdup(char *str)
+{
+	int i;
+	char *p;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	p = malloc((sizeof(char) * strlen(str)) + 1);
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	i = 0;
+	if (*str == '\0')
+	{
+		*p = '\0';
+		return (p);
+	}
+	else
+	{
+		while (*(str + i) != '\0')
+		{
+			*(p + i) = *(str + i);
+			i++;
+		}
+	}
+	return (p);
+}
 /**
   * new_dog - creates a new dog :)
   * @name: name of the dog
@@ -16,13 +50,13 @@ dog_t *new_dog(char *name, float age, char *owner)
 	d = malloc(sizeof(dog_t));
 	if (!d)
 		return (NULL);
-	d->name = strdup(name);
+	d->name = _strdup(name);
 	if (!(d->name))
 	{
 		free(d);
 		return (NULL);
 	}
-	d->owner = strdup(owner);
+	d->owner = _strdup(owner);
 	if (!(d->owner))
 	{
 		free(d->name);
