30e83fe7636f47dbb0ed5b4b2304514cf4f9ee30
Diegoacosta127 <diegoacosta127@gmail.com>
2021-11-01 14:24:34 -0700

    Fixed by Martín

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,29 @@
 #include <stdlib.h>
 #include <string.h>
 /**
+ * _strdup - returns a pointer to a newly allocated space in memory,
+ * @str: string to copy
+ * Return: *ptr to the duplicate string or NULL if it failed
+ */
+char *_strdup(char *str)
+{
+	unsigned int pos, len;
+	char *dup;
+
+	if (!str)
+		return (NULL);
+	for (len = 0; str[len] != 0;)
+		len++;
+	dup = (char *)malloc((sizeof(char) * len + 1));
+	if (dup == NULL)
+		return (NULL);
+	for (pos = 0; pos <= len; pos++)
+	{
+		dup[pos] = str[pos];
+	}
+	return (dup);
+}
+/**
   * new_dog - creates a new dog :)
   * @name: name of the dog
   * @age: age of the puppy
@@ -15,13 +38,13 @@ dog_t *new_dog(char *name, float age, char *owner)
 	d = malloc(sizeof(dog_t));
 	if (!d)
 		return (NULL);
-	d->name = strdup(name);
+	d->name = _strdup(name);
 	if (!(d->name))
 	{
 		free(d);
 		return (NULL);
 	}
-	d->owner = strdup(owner);
+	d->owner = _strdup(owner);
 	if (!(d->owner))
 	{
 		free(d->name);
