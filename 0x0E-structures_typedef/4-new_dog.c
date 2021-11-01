84c175b0ae5c39aacf95489eb733b7589fc5b49c
Diegoacosta127 <diegoacosta127@gmail.com>
2021-11-01 11:52:24 -0700

    Fixed with _strdup

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,6 +3,41 @@
 #include <stdio.h>
 #include <string.h>
 /**
+  * _strdup - returns pointer to newly allocated space in memo
+  * @str: string to be copied
+  * Return: pointer to memo. alloc., NULL if str = NULL or if insufficiente mem
+  */
+char *_strdup(char *str)
+{
+	int i;
+	char *p;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	p = malloc((sizeof(char) * strlen(str)) + 1);
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	i = 0;
+	if (*str == '\0')
+	{
+		*p = '\0';
+		return (p);
+	}
+	else
+	{
+		while (*(str + i) != '\0')
+		{
+			*(p + i) = *(str + i);
+			i++;
+		}
+	}
+	return (p);
+}
+/**
   * new_dog - creates a new dog :)
   * @name: name of the dog
   * @age: age of the puppy
