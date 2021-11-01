7c390ab2bba0975ef7e47b842dd76be55c70b678
Diegoacosta127 <diegoacosta127@gmail.com>
2021-11-01 11:39:34 -0700

    Missing returns added

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -18,12 +18,16 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	d->name = strdup(name);
 	if (!d->name)
+	{
 		free(d);
+		return (NULL);
+	}
 	d->owner = owner;
 	if (!d->owner)
 	{
 		free(d->name);
 		free(d);
+		return (NULL);
 	}
 	d->age = age;
 	return (d);
