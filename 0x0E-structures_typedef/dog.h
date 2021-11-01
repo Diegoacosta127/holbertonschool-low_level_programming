a2497602417be9e5fbde6ba37c45d4030ec44ff4
Diegoacosta127 <diegoacosta127@gmail.com>
2021-11-01 12:13:47 -0700

    Some extra functions added

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,6 +14,7 @@ typedef struct dog
 } dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+int _strlen(char *s);
 char *_strdup(char *str);
 dog_t *new_dog(char *name, float age, char *owner);
 #endif
