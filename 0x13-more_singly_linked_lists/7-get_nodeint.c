4225961feffef1b8543752db452b3506323ba674
Diegoacosta127 <diegoacosta127@gmail.com>
2021-11-16 13:43:08 -0800

    Condition for existence of head changed

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 #include <stdio.h>
 /**
-  * get_nodeint_at_index - returns the nth node of a 'listint_t' linked list
+ * get_nodeint_at_index - returns the nth node of a 'listint_t' linked list
   * @head: pointer to listint_t
   * @index: the index
   * Return: the nth node, NULL if it doesn't exist
@@ -10,14 +10,11 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i;
 
-	if (head)
-	{
-		for (i = 0; i < index; i++)
-		{
-			head = head->next;
-		}
-		return (head);
-	}
-	else
+	if (!head)
 		return (NULL);
+	for (i = 0; i < index; i++)
+	{
+		head = head->next;
+	}
+	return (head);
 }
