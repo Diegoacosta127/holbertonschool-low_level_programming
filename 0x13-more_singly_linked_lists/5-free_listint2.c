b3830712b52f883867b60953910f337685682ab7
Diegoacosta127 <diegoacosta127@gmail.com>
2021-11-16 07:46:33 -0800

    Exit status deleted

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,8 +9,8 @@ void free_listint2(listint_t **head)
 {
 	listint_t *aux;
 
-	if (head == NULL && *head == NULL)
-		exit (0);
+	if (!head && !(*head))
+		*head = NULL;
 	while (*head && head)
 	{
 		aux = (*head)->next;
