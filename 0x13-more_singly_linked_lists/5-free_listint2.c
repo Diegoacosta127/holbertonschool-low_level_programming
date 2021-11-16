633b430aadc9a543392ab7d8202bb2dac9c8a0e2
Diegoacosta127 <diegoacosta127@gmail.com>
2021-11-16 13:38:12 -0800

    Some conditions deleted

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,9 +9,9 @@ void free_listint2(listint_t **head)
 {
 	listint_t *aux;
 
-	if (!head && !(*head))
+	if (!head)
 		return;
-	while (*head && head)
+	while (*head)
 	{
 		aux = (*head)->next;
 		free(*head);
