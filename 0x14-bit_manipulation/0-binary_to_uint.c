b353d014f58bf6255317c2461851070b581f64c0
Diegoacosta127 <diegoacosta127@gmail.com>
2021-11-18 11:08:23 -0800

    Some code fixes

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,50 +1,25 @@
 #include "main.h"
 /**
-  * _strlen - length of a string
-  * @s: pointer to char
-  * Return: length of a string
-  */
-unsigned int _strlen(const char *s)
-{
-	unsigned int i;
-
-	i = 0;
-	while (*s != '\0')
-	{
-		i++;
-		s++;
-	}
-	return (i);
-}
-/**
   * binary_to_uint - converts a binary to an unsigned int
   * @b: pointer to string of 0 and 1 chars
   * Return: the converted number, or 0 if some char isn't 0 or 1, or b is NULL
   */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int len, i, j, pow, sum;
+	unsigned int ret;
+	int i;
 
-	i = 0;
-	if (*(b + i) == '\0')
+	if (!b)
 		return (0);
-	len = _strlen(b);
-	sum = 0;
-	while (*(b + i) != '\0')
-	{
-		if (*(b + i) != '1' && *(b + i) != '0')
+	for (i = 0; *(b + i) != '\0'; i++)
+		if (*(b + i) != '0' && *(b + i) != '1')
 			return (0);
+	ret = 0;
+	for (i = 0; *(b + i) != '\0'; i++)
+	{
+		ret *= 2;
 		if (*(b + i) == '1')
-		{
-			pow = 1;
-			for (j = 0; j < len; j++)
-			{
-				pow = pow * 2;
-			}
-			sum = sum + pow;
-		}
-		i++;
-		len--;
+			ret += 1;
 	}
-	return (sum / 2);
+	return (ret);
 }
