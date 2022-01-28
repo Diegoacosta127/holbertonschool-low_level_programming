c3be25731b8f3fcff768c64e17e867fd25eeba59
Diegoacosta127 <diegoacosta127@gmail.com>
2022-01-28 08:07:25 -0800

    3.>>>ht['betty'] = 'cool'

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+/**
+  * hash_table_set - adds an element to the hash table
+  * @ht: hash table to add or update the key/value
+  * @key: the key (cannot be an empty string)
+  * @value: value associated to key (cannot be an empty string)
+  * Return: 1 if succeed, otherwise 0
+  */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	const unsigned char *dupkey;
+	unsigned long int idxkey;
+	hash_node_t *new_node;
+
+	if (!value || !ht)
+		return 0;
+	dupkey = (const unsigned char *)strdup(key);
+	idxkey = key_index(dupkey, ht->size);
+	new_node = malloc(sizeof(* new_node));
+	if (!new_node)
+	{
+		return 0;
+	}
+	new_node->key = (char *)dupkey;
+	new_node->value = strdup(value);
+	new_node->next = ht->array[idxkey];
+	ht->array[idxkey] = new_node;
+	return 1;
+}
