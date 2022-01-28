c351380506def1ff34506ee7d577abc362db9bac
Diegoacosta127 <diegoacosta127@gmail.com>
2022-01-28 05:48:48 -0800

    some malloc fixed, courtesy by Martín C

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,10 +12,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (size == 0)
 		return (NULL);
 	if (!new_ht)
-	{
 		return (NULL);
-	}
-	new_ht->array = malloc(size * sizeof(hash_node_t));
+	new_ht->array = malloc(sizeof(hash_node_t *) * size);
 	if (!new_ht->array)
 	{
 		free(new_ht);
