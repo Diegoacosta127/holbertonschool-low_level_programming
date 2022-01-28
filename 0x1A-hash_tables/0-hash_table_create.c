aaef878eb1d0aea486f27d71e47df92ed69494a5
Diegoacosta127 <diegoacosta127@gmail.com>
2022-01-28 04:25:11 -0800

    Betty style fixes

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,18 +11,18 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	if (!new_ht)
 	{
-		return NULL;
+		return (NULL);
 	}
-	new_ht->array = malloc(size*sizeof(hash_node_t));
+	new_ht->array = malloc(size * sizeof(hash_node_t));
 	if (!new_ht->array)
 	{
 		free(new_ht);
-		return NULL;
+		return (NULL);
 	}
 	new_ht->size = size;
-	for(i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		new_ht->array[i] = NULL;
 	}
-	return new_ht;
+	return (new_ht);
 }
