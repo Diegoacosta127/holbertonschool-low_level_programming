3e50bb44ca6df387f2d11a7d1966224c7e082b60
Diegoacosta127 <diegoacosta127@gmail.com>
2022-01-28 12:22:55 -0800

    Betty fixes

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -19,7 +19,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (ht->array[idxkey])
 	{
 		new_node = ht->array[idxkey];
-		while(new_node)
+		while (new_node)
 		{
 			if (new_node->key == key)
 			{
