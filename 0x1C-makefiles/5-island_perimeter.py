bd94d6e665aa76d8f2786e537b9c6feb629876bd
Diegoacosta127 <diegoacosta127@gmail.com>
2022-03-17 14:52:39 -0700

    now it works, but Python has a logical lack

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,16 +8,13 @@ def island_perimeter(grid):
 
     for i in range(len(grid)):
         for j in range(len(grid[i])):
-            try:
-                if grid[i][j] == 1:
-                    if grid[i - 1][j] == 0 or i == 0:
-                        perimeter += 1
-                    if grid[i + 1][j] == 0 or i == len(grid) - 1:
-                        perimeter += 1
-                    if grid[i][j - 1] == 0 or j == 0:
-                        perimeter += 1
-                    if grid[i][j + 1] == 0 or j == len(grid) - 1:
-                        perimeter += 1
-            except Exception as ex:
-                pass
+            if grid[i][j] == 1:
+                if grid[i - 1][j] == 0 or i == 0:
+                    perimeter += 1
+                if i == len(grid) - 1 or grid[i + 1][j] == 0:
+                    perimeter += 1
+                if grid[i][j - 1] == 0 or j == 0:
+                    perimeter += 1
+                if j == len(grid[i]) - 1 or grid[i][j + 1] == 0:
+                    perimeter += 1
     return perimeter
