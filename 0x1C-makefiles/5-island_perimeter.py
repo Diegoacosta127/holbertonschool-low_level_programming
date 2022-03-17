57119b62a6ccdc5510edb78e2f7ff138c4af3ee7
Diegoacosta127 <diegoacosta127@gmail.com>
2022-03-17 13:25:48 -0700

    exception added

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -6,15 +6,18 @@ def island_perimeter(grid):
     """island perimeter doc"""
     perimeter = 0
 
-    for i in range(1, len(grid)):
-        for j in range(1, len(grid[i])):
-            if grid[i][j] == 1:
-                if grid[i - 1][j] == 0:
-                    perimeter += 1
-                if grid[i + 1][j] == 0:
-                    perimeter += 1
-                if grid[i][j - 1] == 0:
-                    perimeter += 1
-                if grid[i][j + 1] == 0:
-                    perimeter += 1
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            try:
+                if grid[i][j] == 1:
+                    if grid[i - 1][j] == 0:
+                        perimeter += 1
+                    if grid[i + 1][j] == 0:
+                        perimeter += 1
+                    if grid[i][j - 1] == 0:
+                        perimeter += 1
+                    if grid[i][j + 1] == 0:
+                        perimeter += 1
+            except Exception as ex:
+                pass
     return perimeter
